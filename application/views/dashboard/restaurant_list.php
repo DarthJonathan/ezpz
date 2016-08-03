09ddc03ca119541f6dfbff4945e8b7314cf0e67f
setyawan9 <setyawansusanto99@outlook.com>
2016-08-03 17:36:50 +0700

    restaurant

--- application/views/dashboard/restaurant_list.php
+++ application/views/dashboard/restaurant_list.php
@@ -1,76 +1,70 @@
 <div class="container">
-	
-	<div class="col-md-1 col-xs-12"></div>
 
-			<div class="col-md-10 col-xs-12" style="background-color:#ccc;">
+			<h2>
+				---Section Title--- Cuisine
+			</h2>
+			<hr>	
 
-					<h2>
-						---Section Title--- Cuisine
-					</h2>
-					<hr>	
+			<div class="col-md-4 col-xs-12">
 
-					<div class="col-md-4 col-xs-12 column-card">
+				<div class="card">	
 
-						<div class="card">	
+					<div class="image">	
+						
+						<i class="fa fa-picture-o fa-4x"></i>
+						
+					</div>
 
-							<div class="image">	
-								
-								<i class="fa fa-picture-o fa-5x"></i>
-								
-							</div>
-
-							<div class="panel-body card-body">
-									<p>Restaurant Brand</p>
-									<p>Address</p>
-									<p>Open</p>
-							</div>
-							
-						</div>
-
+					<div class="panel-body card-body">
+							<p class="brand">Restaurant Brand</p>
+							<p>Address</p>
+							<p>Open</p>
 					</div>
 					
-					<div class="col-md-4 col-xs-12 column-card">
+				</div>
 
-						<div class="card">	
+			</div>
+			
+			<div class="col-md-4 col-xs-12">
 
-							<div class="image">	
-								
-								<i class="fa fa-picture-o fa-4x"></i>
-								
-							</div>
+				<div class="card">	
 
-							<div class="panel-body card-body">
-									<p>Restaurant Brand</p>
-									<p>Address</p>
-									<p>Open</p>
-							</div>
-							
-						</div>
-
+					<div class="image">	
+						
+						<i class="fa fa-picture-o fa-4x"></i>
+						
 					</div>
 
-					<div class="col-md-4 col-xs-12 column-card">
-
-						<div class="card">	
-
-							<div class="image">	
-								
-								<i class="fa fa-picture-o fa-5x"></i>
-								
-							</div>
-
-							<div class="panel-body card-body">
-									<p>Restaurant Brand</p>
-									<p>Address</p>
-									<p>Open</p>
-							</div>
-							
-						</div>
-
+					<div class="panel-body card-body">
+							<p class="brand">Restaurant Brand</p>
+							<p>Address</p>
+							<p>Open</p>
 					</div>
+					
+				</div>
 
 			</div>
 
-	<div class="col-md-1 col-xs-12"></div>
+			<div class="col-md-4 col-xs-12">
+
+				<div class="card">	
+
+					<div class="image">	
+						
+						<i class="fa fa-picture-o fa-4x"></i>
+						
+					</div>
+
+					<div class="panel-body card-body">
+							<p class="brand">Restaurant Brand</p>
+							<p>Address</p>
+							<p>Open</p>
+					</div>
+					
+				</div>
+
+			</div>
+
+	</div>
 
 </div>
