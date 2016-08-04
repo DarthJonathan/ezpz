5e8852c7f98efe4de334c468f48e68e28674b777
setyawan9 <setyawansusanto99@outlook.com>
2016-08-04 16:42:56 +0700

    Minor

--- application/controllers/Restaurant.php
+++ application/controllers/Restaurant.php
@@ -20,7 +20,6 @@
 
 			$this->load->view('template/footer');
 		}
-
 		
 
 	}
---
+++ application/views/restaurant/restaurant_details.php
@@ -0,0 +1,54 @@
+<div class="container-fluid image-full-section">
+	<div class="row">
+		<form role="form">
+		<div class="form-group center-block">
+			<div class="input-group">
+				<input type="text" name="restaurant-search" class="form-control" id="restaurant-search" placeholder="Search for restaurant name">
+				<span class="input-group-btn">
+ 				<button class="btn btn-default" type="submit">
+          		<span class="glyphicon glyphicon-search"></span>
+			</div>
+		</div>
+	</form>
+	</div>
+</div>
+<div class="container-fluid padding-top-five">
+	<h2 style="margin-left:1%;">Section Name</h2>
+	<hr></hr>
+	<div class="col-md-4 col-xs-12">
+		<div class="panel panel-default panel-horizontal">
+		    <div class="panel-body">
+		        <p class="brand">Restaurant Brand</p>
+				<p>Address</p>
+				<p>Open</p>
+		    </div>
+		    <div class="panel-heading">
+		        <p>Image</p>
+		    </div>
+		</div>
+	</div>
+	<div class="col-md-4 col-xs-12">
+		<div class="panel panel-default panel-horizontal">
+		    <div class="panel-body">
+		        <p class="brand">Restaurant Brand</p>
+				<p>Address</p>
+				<p>Open</p>
+		    </div>
+		    <div class="panel-heading">
+		        <p>Image</p>
+		    </div>
+		</div>
+	</div>
+	<div class="col-md-4 col-xs-12">
+		<div class="panel panel-default panel-horizontal">
+		    <div class="panel-body">
+		        <p class="brand">Restaurant Brand</p>
+				<p>Address</p>
+				<p>Open</p>
+		    </div>
+		    <div class="panel-heading">
+		        <p>Image</p>
+		    </div>
+		</div>
+	</div>
+</div>
