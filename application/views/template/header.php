e380e03b2a09a944e5610c106e35886b2268fc0e
setyawan9 <setyawansusanto99@outlook.com>
2016-08-05 09:42:32 +0700

    Font Awesome

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -11,7 +11,7 @@
     <link href="<?php echo base_url() ?>css/bootstrap-theme.min.css" type="text/css" rel="stylesheet">
     <link href="<?php echo base_url() ?>css/flat-ui.min.css" type="text/css" rel="stylesheet">
     <link href="<?php echo base_url() ?>css/custom.css" type="text/css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
     <!-- Begin Scripts -->
 	<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
     <script src="<?php echo base_url() ?>js/flat-ui.min.js"></script>
