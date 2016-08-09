ce8f7977c06a70f124a976d33bc21ebc7ff46482
whadever <irpanwinata@gmail.com>
2016-08-09 09:02:18 +0700

    template changes

--- application/views/templates/default_admin.php
+++ application/views/templates/default_admin.php
@@ -124,47 +124,159 @@
 			  -webkit-transform: translateY(-3px);
 			  transform: translateY(-3px);
 			}
+
+			/* The side navigation menu */
+			.sidenav {
+			    height: 100%; /* 100% Full-height */
+			    width: 0; /* 0 width - change this with JavaScript */
+			    position: fixed; /* Stay in place */
+			    z-index: 1; /* Stay on top */
+			    top: 0;
+			    left: 0;
+			    background-color: #111; /* Black*/
+			    overflow-x: hidden; /* Disable horizontal scroll */
+			    padding-top: 60px; /* Place content 60px from the top */
+			    transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+			}
+
+			/* The navigation menu links */
+			.sidenav a {
+			    padding: 8px 8px 8px 32px;
+			    text-decoration: none;
+			    font-size: 25px;
+			    color: #818181;
+			    display: block;
+			    transition: 0.3s
+			}
+
+			/* When you mouse over the navigation links, change their color */
+			.sidenav a:hover, .offcanvas a:focus{
+			    color: #f1f1f1;
+			}
+
+			/* Position and style the close button (top right corner) */
+			.sidenav .closebtn {
+			    position: absolute;
+			    top: 0;
+			    right: 25px;
+			    font-size: 36px;
+			    margin-left: 50px;
+			}
+
+			/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+			#main {
+			    transition: margin-left .5s;
+			    padding: 20px;
+			}
+
+			/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+			@media screen and (max-height: 450px) {
+			    .sidenav {padding-top: 15px;}
+			    .sidenav a {font-size: 18px;}
+			}
+
+			#menu-button{
+			
+			background-color: transparent;
+			border : 1px solid #CCC;
+			border-radius: 5px;
+			
+
+			}
+
+			#menu-button:hover{
+				background-color: #FFF12C;
+				-webkit-transition: all 1s ease;
+				-moz-transition: all 0.6s ease;
+				-ms-transition: all 0.6s ease;
+				-o-transition: all 0.6s ease;
+				transition: all 0.6s ease;
+			}
+
+			#background-btn{
+
+				padding-top: 50px;
+
+			}
+
+			
+			.bottom-align-text {
+			    position: absolute;
+			    bottom: 0;
+			    width: 100%;
+			    font-size: 10px;
+		    }
+
+		    .sidenav a{
+		    	font-size: 18px;
+		    	border-bottom: 1px solid black;
+		    }
 	
 	</style>
     
 </head>
 
 <body>
-<!--NavBar-->
-<div class="container-fluid">
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-	  <div class="container-fluid">
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span> 
-	      </button>
-	      <a class="navbar-brand" href="#">EZPZ</a>
-	    </div>
-	    <div class="collapse navbar-collapse" id="myNavbar">
-	      <ul class="nav navbar-nav navbar-left">
-	        <li class="active"><a href="<?php echo base_url('admin') ?>" class="nav-link">Home</a></li>
-	        <li><a href="#" class="nav-link">About Us</a></li>
-	        <li><a href="#" class="nav-link">Restaurants</a></li> 
-	        <li><a href="#" class="nav-link">Top Up Wallet</a></li> 
-	      </ul>
-	      <ul class="nav navbar-nav navbar-right">
-	      	<?php if($this->session->userdata('admin_isLogged') == FALSE): ?>
-	        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-	        <?php else: ?>
-	        	<li><a href="#">Welcome, Reyner !</a></li>
-	        	<li><a href="<?php echo base_url('admin/logout') ?>"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-	        <?php endif; ?>
-	      </ul>
-	    </div>
+
+<div id="mySidenav" class="sidenav">
+<div class="company_logo" style="margin-bottom: 30px; padding-left: 32px">
+	  	<h2>EZPZ</h2>
 	  </div>
-	</nav>
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Drivers</a>
+  <a href="#">Users</a>
+  <a href="#">Partners</a>
+  <a href="#">Unapproved Partners</a>
+  <a href="<?php echo base_url('admin/logout') ?>"><span class="glyphicon glyphicon-log-in"></span> Logout</a>
 </div>
-<!--Navbar End-->
+
+<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+<div id="main">
+	<div class="row">
+	  	<div class="col-xs-2">
+	  		<div id="background-btn" class="col-xs-4">
+				<button id="menu-button" onclick="openNav()"><img src="<?php echo base_url() ?>assets/menu.png" width="30" alt="">
+				</button>
+			</div>
+	  	</div>
+	  	<div class="col-xs-8">
+		  	<div class="row">
+		  		<div class="col-sm-4"></div>
+		  		<div class="col-sm-4 text-center">
+		  			<a href="<?php echo base_url('home') ?>"><img src="<?php echo base_url() ?>assets/rwlogo.jpg" style="margin-bottom:10px; padding-top: 20px;width:100%" class="img img-responsive" alt="">
+		  			</a>
+		  		</div>
+		  		<div class="col-sm-4">
+		  			
+		  		</div>
+	  		</div>
+	  	</div>
+	  	<div class="col-xs-2"><h5 style="padding-top: 50px" class="pull-right">Welcome, <?php echo $this->session->userdata('admin_username') ?></h5></div>
+	  </div>
+	  <hr>
+	  <div class="row">
+			<div class="col-xs-12">
+				<?php echo $body ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+</script>
 				
-<?php echo $body ?>
+
 
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
