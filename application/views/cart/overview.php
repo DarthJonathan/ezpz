f4d9458ee3622332b68cdd284179882cf7911608
felitas <felita.31895@gmail.com>
2016-08-15 13:22:37 +0700

    ganti2 dikit

--- application/views/cart/overview.php
+++ application/views/cart/overview.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 <div class="modal-dialog" role="document" style="width:60%">
-<div class="modal-content login">
-<div class="modal-header heading">
+<div class="modal-content">
+<div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="myModalLabel">Food Cart</h4>
 </div>
