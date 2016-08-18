f29b204d987437dcc92435c1233bf0b885ad933c
Jonathan Hosea <jonathan.hosea@me.com>
2016-08-18 17:19:38 +0700

    orders

--- database_SQL_Dumps/order_detail.sql
+++ database_SQL_Dumps/order_detail.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 11, 2016 at 05:50 AM
+-- Generation Time: Aug 18, 2016 at 12:18 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -30,11 +30,40 @@ CREATE TABLE `order_detail` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `options` varchar(255) NOT NULL,
+  `options` varchar(255) DEFAULT NULL,
   `qty` int(11) NOT NULL,
   `sub_total` int(11) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`id`, `order_id`, `product_id`, `options`, `qty`, `sub_total`, `created`) VALUES
+(17, 18, 1, NULL, 3, 30000, '2016-08-12 02:36:44'),
+(18, 18, 2, NULL, 2, 40000, '2016-08-12 02:36:44'),
+(19, 19, 1, NULL, 30, 3000, '2016-08-12 03:55:44'),
+(20, 20, 1, NULL, 30, 3000, '2016-08-12 03:56:20'),
+(21, 21, 1, NULL, 30, 3000, '2016-08-12 03:58:13'),
+(22, 22, 1, NULL, 30, 3000, '2016-08-12 03:59:08'),
+(23, 23, 1, NULL, 30, 3000, '2016-08-12 04:00:23'),
+(24, 24, 1, NULL, 30, 3000, '2016-08-12 04:00:48'),
+(25, 25, 1, NULL, 30, 3000, '2016-08-12 04:01:14'),
+(26, 26, 2, NULL, 100, 15000, '2016-08-12 06:51:16'),
+(27, 27, 2, NULL, 300, 45000, '2016-08-12 06:52:26'),
+(28, 28, 1, NULL, 20, 2000, '2016-08-12 06:59:30'),
+(29, 29, 1, NULL, 20, 2000, '2016-08-12 06:59:31'),
+(30, 30, 1, NULL, 3, 300, '2016-08-12 07:01:08'),
+(31, 31, 1, NULL, 3, 300, '2016-08-12 07:01:08'),
+(32, 32, 1, NULL, 3, 300, '2016-08-12 07:02:08'),
+(33, 33, 1, NULL, 3, 300, '2016-08-12 07:04:18'),
+(34, 34, 1, NULL, 3, 300, '2016-08-12 07:05:11'),
+(35, 35, 1, NULL, 3, 300, '2016-08-12 07:05:13'),
+(36, 36, 1, NULL, 3, 300, '2016-08-12 07:05:36'),
+(37, 37, 1, NULL, 3, 300, '2016-08-12 07:06:11'),
+(38, 38, 1, NULL, 3, 300, '2016-08-12 07:08:46'),
+(39, 39, 1, NULL, 4, 400, '2016-08-12 07:09:44');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +83,7 @@ ALTER TABLE `order_detail`
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- database_SQL_Dumps/order_history.sql
+++ database_SQL_Dumps/order_history.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 11, 2016 at 05:50 AM
+-- Generation Time: Aug 18, 2016 at 12:18 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -38,6 +38,51 @@ CREATE TABLE `order_history` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `order_history`
+--
+
+INSERT INTO `order_history` (`id`, `status`, `code`, `user_id`, `total_product`, `total_qty`, `total_price`, `delivery_cost`, `created`) VALUES
+(18, 0, 'a7322', 33, 2, 5, 70000, 0, '2016-08-12 02:36:44'),
+(20, 1, '36472', 33, 1, 30, 3000, 0, '2016-08-12 03:56:20'),
+(21, 0, '481dd', 33, 1, 30, 3000, 0, '2016-08-12 03:58:13'),
+(22, 0, '0dfca', 33, 1, 30, 3000, 0, '2016-08-12 03:59:08'),
+(23, 0, 'a131c', 33, 1, 30, 3000, 0, '2016-08-12 04:00:23'),
+(24, 0, '2db8c', 33, 1, 30, 3000, 0, '2016-08-12 04:00:48'),
+(25, 1, '61b42', 33, 1, 30, 3000, 0, '2016-08-12 04:01:14'),
+(27, 0, 'a02df', 33, 1, 300, 45000, 0, '2016-08-12 06:52:26'),
+(28, 0, '103c6', 33, 1, 20, 2000, 0, '2016-08-12 06:59:30'),
+(29, 1, '717ae', 33, 1, 20, 2000, 0, '2016-08-12 06:59:31'),
+(30, 0, '48f52', 33, 1, 3, 300, 0, '2016-08-12 07:01:08'),
+(31, 1, 'b2279', 33, 1, 3, 300, 0, '2016-08-12 07:01:08'),
+(32, 0, '28bb7', 33, 1, 3, 300, 0, '2016-08-12 07:02:08'),
+(33, 0, '6f3a7', 33, 1, 3, 300, 0, '2016-08-12 07:04:18'),
+(34, 0, '5285d', 33, 1, 3, 300, 0, '2016-08-12 07:05:11'),
+(35, 0, '51000', 33, 1, 3, 300, 0, '2016-08-12 07:05:13'),
+(36, 0, '29574', 33, 1, 3, 300, 0, '2016-08-12 07:05:36'),
+(37, 0, 'd1d2d', 33, 1, 3, 300, 0, '2016-08-12 07:06:11'),
+(38, 0, '954f4', 33, 1, 3, 300, 0, '2016-08-12 07:08:46'),
+(39, 1, 'c7023', 33, 1, 4, 400, 0, '2016-08-12 07:09:44');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order_history`
+--
+ALTER TABLE `order_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order_history`
+--
+ALTER TABLE `order_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
